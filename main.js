059c0c8e2ba249cf905b14ac0c4679a69462f8c8
Zidan Naufal <83897266+zdnpl@users.noreply.github.com>
2024-12-06 23:22:14 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  const aboutSection = document.querySelector(".about");
+  const skillsSection = document.querySelector(".skills");
+  const portoSection = document.querySelector(".portfolio");
+
+  const aboutPosition = aboutSection.getBoundingClientRect().top + window.scrollY;
+  const skillsPosition = skillsSection.getBoundingClientRect().top + window.scrollY;
+  const portoPosition = portoSection.getBoundingClientRect().top + window.scrollY;
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition >= portoPosition) {
+    // Bagian portfolio - navbar berubah putih
+    navbar.classList.add("navbar-light-bg", "navbar-light");
+    navbar.classList.remove("navbar-dark-bg", "navbar-dark");
+  } else if (scrollPosition >= skillsPosition) {
+    // Bagian skills - navbar berubah hitam
+    navbar.classList.add("navbar-dark-bg", "navbar-dark");
+    navbar.classList.remove("navbar-light-bg", "navbar-light");
+  } else if (scrollPosition >= aboutPosition) {
+    // Bagian about - navbar berubah putih
+    navbar.classList.add("navbar-light-bg", "navbar-light");
+    navbar.classList.remove("navbar-dark-bg", "navbar-dark");
+  } else {
+    // Bagian lain - navbar transparan
+    navbar.classList.add("navbar-dark-bg", "navbar-dark");
+    navbar.classList.remove("navbar-light-bg", "navbar-light");
+  }
+});
